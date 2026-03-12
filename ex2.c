f76f1c584db915eb3f17036c58490e198728bb2c
davivi-py <davialcantaralim@gmail.com>
2026-03-12 15:24:45 -0300

    commit usando WSL

--- ex2.c
+++ ex2.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-int main(){
-
-    int scanf(
-
-
+int main()
+{
+    printf("Digite uma temperatura em Celsius: ");
+    float celsius;
+    scanf("%f", &celsius);
+    float fahrenheit = (celsius * 9) / 5 + 32;
+    printf("Esta temperatura na escala Fahrenheit é: %2f\n", fahrenheit);
+    return 0;
 }
